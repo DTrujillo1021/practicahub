5ef32234e5f1331bfc3afddb25fbdc5923fd4c0e
Alvarez8571 <00026626@uca.edu.sv>
2026-05-25 15:26:36 -0600

    cambio

--- praticabranch.cpp
+++ praticabranch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int opcion;
 int main(){
-    cout<<"ingrese un numero del 1 al 7: ";
+    cout<<"ingrese un numero del 1 al 8: ";
     cin>>opcion;
     switch(opcion)
         {
@@ -34,6 +34,12 @@ int main(){
             case 7: 
                 cout<<"El dia es Domingo"<<endl;
                 break;
+            
+            case 8:
+                cout<<"algo";
+                break ;
+
+              
             
             default:
                 cout<<"La opcion no es valida"<<endl;
