e91ac61aa2a62503429effebb1651edfb12f61b9
DTrujillo1021 <00094726@uca.edu.sv>
2026-05-25 15:09:23 -0600

    semanas

---
+++ praticabranch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int opcion;
+int main(){
+    cout<<"ingrese un numero del 1 al 7: ";
+    cin>>opcion;
+    switch(opcion)
+        {
+            case 1:
+                cout<<"El dia es lunes"<<endl;
+                break;
+
+            case 2:
+                cout<<"El dia es Martes"<<endl;
+                break;
+
+            case 3:
+                cout<<"El dia es Miercoles"<<endl;
+                break;
+
+
+            case 4: 
+                cout<<"El dia es Jueves"<<endl;
+                break;
+
+            case 5: 
+                cout<<"El dia es Viernes"<<endl;
+                break;
+
+            case 6:
+                cout<<"El dia es Sabado"<<endl;
+                break;
+
+            case 7: 
+                cout<<"El dia es Domingo"<<endl;
+                break;
+            
+            default:
+                cout<<"La opcion no es valida"<<endl;
+                break;}
+            
+return 0; }
